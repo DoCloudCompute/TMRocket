925cef1a55ed52d68b816542fc8bf526ffa3d60a
Dorian <dodo.sc1508@gmail.com>
2020-05-22 15:39:23 +0200

    Preliminary rendition

--- progs/flight_pred_mk1.py
+++ progs/flight_pred_mk1.py
@@ -12,19 +12,20 @@ cons_atm = float(1.01325e5) # in Pa
 height = 0 # in meters
 mass = 0.2 # in kg
 speed = 0 # in m/s
-nozzle_area = math.pi * 0.017**2
+tank_capacity = 1.5 # in liters
+nozzle_area = math.pi * 0.011**2
 
-drag_coeff = 0.27 # drag coefficient given by CFD
+drag_coeff = 0.75 # drag coefficient given by CFD
 vis_section = math.pi * 0.05**2 # apparent section of the rocket (5 cm radius in this case)
 
 fuel_rho = 998 # in g/L (or kg/m³)
-fuel_vol = 1 # in liters
-init_fuel_vol = fuel_vol
+fuel_vol = int(input("fuel %? [30%]") or 30)
+#fuel_vol =  30 # in % of the tank's capacity
 
-tank_pres = 10*cons_atm
-tank_vol = 0.2 # liters of compressed air
+tank_pres = 6*cons_atm
+tank_vol = 100-fuel_vol # in % of the tank's capacity
 
-delta_t = 0.005 # in secs
+delta_t = 0.00005 # in secs
 sim_time = 0 # in secs
 max_time = 10 # in secs
 
@@ -34,7 +35,13 @@ pllst1 = []
 pllst2 = []
 pllst3 = []
 pllst4 = []
+optlst = []
 
+fuel_vol = (fuel_vol/100) * tank_capacity
+init_fuel_vol = fuel_vol
+print(fuel_vol)
+tank_vol = (tank_vol/100) * tank_capacity
+print(tank_vol)
 
 # funcs
 def get_curr_grav(height):
@@ -90,7 +97,9 @@ def get_disp(push_force, fuel_mass, mass, delta_t, height, speed):
 	acc = push_force/r_mass
 	disp = (0.5 * acc * delta_t**2) + (speed*delta_t) + (height) # x = 0.5.at²+x0
 	return max([0, disp]) # clamp min at 0
-	
+
+capped = False
+
 # time loop	
 while sim_time < max_time:
 	grav = get_curr_grav(height)
@@ -104,7 +113,9 @@ while sim_time < max_time:
 	height = get_disp(push_force, fuel_mass, mass, delta_t, height, speed)
 	speed = get_spd(push_force, fuel_mass, mass, delta_t, speed, height)
 	
-	if height == 0 and sim_time != 0: break
+	if height == 0 and sim_time != 0 and not capped:
+		max_time = sim_time + 1
+		capped = True
 	
 	timelst.append(sim_time)
 	pllst1.append(speed)
@@ -115,9 +126,11 @@ while sim_time < max_time:
 
 print("{}% of fuel left | air tank pressure: {} atm | apogee: {}m | max speed: {}m/s".format(round(100*fuel_vol/init_fuel_vol, 2), round(tank_pres/cons_atm, 2), round(max(pllst3),2), round(max(pllst1),2)))
 
+"""
 plt.plot(timelst, pllst1, label="speed")
 plt.plot(timelst, pllst2, label="drag")
 plt.plot(timelst, pllst3, label="height")
 plt.plot(timelst, pllst4, label="thrust")
 plt.legend()
 plt.show()
+"""
